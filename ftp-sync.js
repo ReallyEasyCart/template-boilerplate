e8be4280c0ddb05354622269311fdd6631d43433
Andrew Stilliard <andrew.stilliard@gmail.com>
2016-01-05 15:47:53 +0000

    ftp-sync.js file updated to handle location of .remote-sync in parent folder (incase downloading all templates), also now handling initial start of watch for changes better skipping the initial few times it shows when starting, and finally cleaning up nicer on ctrl+c

--- ftp-sync.js
+++ ftp-sync.js
@@ -1,10 +1,39 @@
+
 'use strict';
 
+const path = require('path');
 const chokidar = require('chokidar');
 const sass = require('node-sass');
 const fs = require('fs');
 const ftpClient = require('ftp');
 let ftpConnection = new ftpClient();
+
+// collect FTP details
+let ftpConfig = null;
+try {
+    // try in current folder
+    ftpConfig = fs.readFileSync('.remote-sync.json');
+}
+catch (e) {
+    // else try in above folder
+    try {
+        ftpConfig = fs.readFileSync('../.remote-sync.json');
+    }
+    catch (e) {
+        console.log('Please setup .remote-sync.json config file');
+        process.exit(1);
+    }
+}
+
+ftpConfig = JSON.parse(ftpConfig);
+if ( ! ftpConfig) {
+    console.log('Remote-sync config file is not valid JSON');
+    process.exit(1);
+}
+// root target? (well we still just want to sync this folder, so take this folders name instead)
+if (ftpConfig.target == '/') {
+    ftpConfig.target = '/' + path.basename(__dirname);
+}
 
 // sass compile to css function
 function renderSassToCss() {
@@ -48,17 +77,11 @@ function ftpUploadFile(path) {
 }
 
 // await connection before watching / compiling any files
+let ftpConnected = false;
 ftpConnection.on('ready', () => {
 
-    // begin watching directories
-    chokidar.watch('css/**/*.scss', {
-        ignored: /[\/\\]\./,
-        persistent: true,
-        ignoreInitial: true // don't fire on existing files, only changes from now onwards
-    }).on('all', (event, path) => {
-        // console.log(event, path);
-        renderSassToCss();
-    });
+    // we're connected :D
+    ftpConnected = true;
 
     // initial sass compile (incase of changes before this was run)
     renderSassToCss();
@@ -67,16 +90,31 @@ ftpConnection.on('ready', () => {
     console.log('Connected & watching for sass file changes to upload.');
 
 });
+// ftpConnection.on('greeting', (msg) => {
+//     console.log('FTP greeting: ', msg);
+// });
+ftpConnection.on('close', () => {
+    console.log('FTP connection closed');
+});
+ftpConnection.on('error', (err) => {
+    console.log('FTP Error: ', err);
+});
+ftpConnection.on('end', () => {
+    console.log('FTP connection ended');
+    process.exit(1);
+});
 
-// collect FTP details
-let ftpConfig = fs.readFileSync('.remote-sync.json');
-if ( ! ftpConfig) {
-    console.log('Please setup remote-sync config file');
-}
-ftpConfig = JSON.parse(ftpConfig);
-if ( ! ftpConfig) {
-    console.log('Remote-sync config file is not valid JSON');
-}
+// begin watching directories
+chokidar.watch('css/**/*.scss', {
+    ignored: /[\/\\]\./,
+    persistent: true,
+    ignoreInitial: true // don't fire on existing files, only changes from now onwards
+}).on('all', (event, path) => {
+    // console.log(event, path);
+    if (ftpConnected) {
+        renderSassToCss();
+    }
+});
 
 // connect to FTP
 ftpConnection.connect({
@@ -85,4 +123,14 @@ ftpConnection.connect({
     password: ftpConfig.password,
     port: ftpConfig.port || 21
 });
-console.log('Connecting to FTP account...');
+console.log('Connecting to FTP account... Ctrl+c to close cleanly, and again to force exit.');
+
+// cleanly close ftp on process exit (Ctrl+c)
+let isClosing = false;
+process.on('SIGINT', function() {
+    ftpConnection.end();
+    if (isClosing) { // on double Ctrl+c, really exit
+        process.exit(1);
+    }
+    isClosing = true;
+});
